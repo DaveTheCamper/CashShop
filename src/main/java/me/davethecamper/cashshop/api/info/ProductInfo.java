eb393a13de1ae46543533e8b86b59672f5331840
DaveTheCamper <thedavewarrior@gmail.com>
2024-01-09 18:28:52 -0300

    [ATS-002] Adicionando getter no totalCash

--- src/main/java/me/davethecamper/cashshop/api/info/ProductInfo.java
+++ src/main/java/me/davethecamper/cashshop/api/info/ProductInfo.java
@@ -1,33 +1,20 @@
 package me.davethecamper.cashshop.api.info;
 
+import lombok.Getter;
+
+@Getter
 public class ProductInfo {
 	
 	public ProductInfo(int totalCash, double amount, String product_name, String currency) {
 		this.amount = amount;
-		this.product_name = product_name;
+		this.productName = product_name;
 		this.currency = currency;
 		this.totalCash = totalCash;
 	}
-
 	
-	private String product_name, currency;
+	private String productName, currency;
 	
 	private double amount;
 
 	private int totalCash;
-
-	
-	
-	public String getProductName() {
-		return product_name;
-	}
-	
-	public String getCurrency() {
-		return currency;
-	}
-
-	public double getAmount() {
-		return amount;
-	}
-
 }
